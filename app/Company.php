8d19494dcb3bad25b70a18cec4dcfe355cc4e967
Marzieh <marzieh.tajik@zoodfood.com>
2021-10-21 11:06:50 +0330

    added model relationships and json serialize

--- app/Company.php
+++ app/Company.php
@@ -4,7 +4,39 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Company extends Model
+class Company extends Model implements \JsonSerializable
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
 
+    public function stations()
+    {
+        return $this->hasMany(Station::class);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Company::class, 'parent_company_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Company::class, 'parent_company_id');
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'createdAt' => $this->created_at->toAtomString(),
+            'updatedAt' => $this->updated_at->toAtomString(),
+        ];
+    }
 }
--- app/Station.php
+++ app/Station.php
@@ -4,7 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Station extends Model
+class Station extends Model implements \JsonSerializable
 {
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 
+    public function jsonSerialize()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'createdAt' => $this->created_at->toAtomString(),
+            'updatedAt' => $this->updated_at->toAtomString(),
+            'company' => $this->company()->get(),
+        ];
+    }
 }
